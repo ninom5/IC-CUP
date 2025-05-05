68357f43874a079739b931634284e3d43c785bb3
JosipVojkovic <128196492+JosipVojkovic@users.noreply.github.com>
2025-05-05 20:50:13 +0200

    Add update-car DTO

--- apps/api/src/car/dto/update-car.dto.ts
+++ apps/api/src/car/dto/update-car.dto.ts
@@ -1,4 +1,55 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateCarDto } from './create-car.dto';
+import { IsBoolean, IsEnum, IsOptional, IsString } from 'class-validator';
+import { CarCategory, FuelType } from '@prisma/client';
 
-export class UpdateCarDto extends PartialType(CreateCarDto) {}
+export class UpdateCarDto extends PartialType(CreateCarDto) {
+  @IsString({ message: 'Vehicle type ID must be a string' })
+  @IsOptional()
+  @ApiProperty({
+    description: 'Vehicle type ID',
+    example: '12345678-1234-1234-1234-123456789012',
+    type: 'string',
+  })
+  vehicleTypeId?: string;
+
+  @IsString({ message: 'License plate must be a string' })
+  @IsOptional()
+  @ApiProperty({
+    description: 'License plate',
+    example: 'ABC1234',
+    type: 'string',
+  })
+  licensePlate?: string;
+
+  @IsEnum(FuelType, {
+    message: `Fuel type must be one of the following: ${Object.values(FuelType).join(', ')}`,
+  })
+  @IsOptional()
+  @ApiProperty({
+    description: 'Fuel type',
+    enum: FuelType,
+    example: FuelType.PETROL,
+  })
+  fuelType?: FuelType;
+
+  @IsBoolean({ message: 'isAutomatic must be a boolean' })
+  @IsOptional()
+  @ApiProperty({
+    description: 'Information if the car is automatic',
+    example: true,
+    type: 'boolean',
+  })
+  isAutomatic?: boolean;
+
+  @IsEnum(CarCategory, {
+    message: `Car category must be one of the following: ${Object.values(CarCategory).join(', ')}`,
+  })
+  @IsOptional()
+  @ApiProperty({
+    description: 'Car category',
+    enum: CarCategory,
+    example: CarCategory.SUV,
+  })
+  category?: CarCategory;
+}
