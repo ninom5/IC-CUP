ab6ccb036aea022399c7bf2c9a104a433f2e9d45
JosipVojkovic <128196492+JosipVojkovic@users.noreply.github.com>
2025-05-07 00:23:04 +0200

    Add modules to app.module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,24 +4,18 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { CarModule } from './car/car.module';
-import { LocationModule } from './location/location.module';
-import { ReviewModule } from './review/review.module';
-import { VehicleModule } from './vehicle/vehicle.module';
-import { VehicleTypeModule } from './vehicle-type/vehicle-type.module';
-import { CarModule } from './car/car.module';
-import { LocationModule } from './location/location.module';
-import { ReviewModule } from './review/review.module';
-import { VehicleModule } from './vehicle/vehicle.module';
-import { VehicleTypeModule } from './vehicle-type/vehicle-type.module';
-import { VehicleTypeModule } from './vehicle-type/vehicle-type.module';
 import { VehicleModule } from './vehicle/vehicle.module';
 import { ReviewModule } from './review/review.module';
 import { LocationModule } from './location/location.module';
-import { CarModule } from './car/car.module';
 
 @Module({
-  imports: [UserModule, AuthModule, CarModule, VehicleTypeModule, VehicleModule, ReviewModule, LocationModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    VehicleModule,
+    ReviewModule,
+    LocationModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
