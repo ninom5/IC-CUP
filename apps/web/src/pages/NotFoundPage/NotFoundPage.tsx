ab328ecd1bfb482b0272709d111962f2e227e0a9
ninom5 <nmilas00@fesb.hr>
2025-05-07 00:12:56 +0200

    Add some content to not found page

--- apps/web/src/pages/NotFoundPage/NotFoundPage.tsx
+++ apps/web/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,3 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import "./notFoundPage.css";
+
 export const NotFoundPage = () => {
-  return <div>not found</div>;
+  const navigate = useNavigate();
+
+  return (
+    <section className="not-found-section">
+      <h1>
+        404, <br />
+        PAGE NOT FOUND
+      </h1>
+      <button id="go-back-button" onClick={() => navigate(-1)}>
+        Go back
+      </button>
+    </section>
+  );
 };
