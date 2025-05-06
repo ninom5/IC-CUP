e7de98d4470e6066bbdbe18ee8cad3816e17e5f3
JosipVojkovic <128196492+JosipVojkovic@users.noreply.github.com>
2025-05-07 00:29:36 +0200

    Add JWT secret

--- apps/api/src/location/location.module.ts
+++ apps/api/src/location/location.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LocationService } from './location.service';
 import { LocationController } from './location.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [LocationController],
-  providers: [LocationService],
+  providers: [LocationService, PrismaService],
+  exports: [LocationService],
 })
 export class LocationModule {}
