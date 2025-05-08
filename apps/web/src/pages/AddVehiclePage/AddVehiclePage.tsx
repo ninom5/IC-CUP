b85aa9f2e6acccc995a093438f1b475e44810ee9
JosipVojkovic <128196492+JosipVojkovic@users.noreply.github.com>
2025-05-08 23:27:39 +0200

    Add to AddVehiclePage form steps functionality

--- apps/web/src/pages/AddVehiclePage/AddVehiclePage.tsx
+++ apps/web/src/pages/AddVehiclePage/AddVehiclePage.tsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+import c from "./AddVehiclePage.module.css";
+
 export const AddVehiclePage = () => {
-  return <div>Add Vehicle Page</div>;
+  const [formStep, setFormStep] = useState(1);
+
+  const handleNextStep = () => {
+    setFormStep((prevStep) => prevStep + 1);
+  };
+
+  return (
+    <section>
+      <h1>List your car</h1>
+
+      <div className={c.formSteps}>
+        <div className={c.formStepBar}>
+          <div style={{ width: `${formStep * 25}%` }}></div>
+        </div>
+        <p>{formStep} / 4</p>
+      </div>
+
+      {formStep < 4 ? (
+        <button onClick={handleNextStep}>NEXT</button>
+      ) : (
+        <button>SUBMIT</button>
+      )}
+    </section>
+  );
 };
