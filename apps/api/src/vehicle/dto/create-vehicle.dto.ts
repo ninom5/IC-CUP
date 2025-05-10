1bb9218180320dac0fcc34a65e0924e28f294662
JosipVojkovic <128196492+JosipVojkovic@users.noreply.github.com>
2025-05-10 20:50:43 +0200

    Add numOfSeats, pickupAddress, longitude, latitide, city and registrationExipration to schema

--- apps/api/src/vehicle/dto/create-vehicle.dto.ts
+++ apps/api/src/vehicle/dto/create-vehicle.dto.ts
@@ -144,6 +144,52 @@ export class CreateVehicleDto {
   })
   vehicleType: VehicleType;
 
+  @IsNotEmpty({ message: 'Address is required' })
+  @IsString({ message: 'Address must be a string' })
+  @ApiProperty({
+    description: 'Address',
+    example: '123 Main St, Springfield, USA',
+    type: 'string',
+  })
+  pickupAddress: string;
+
+  @IsString({ message: 'City must be a string' })
+  @IsOptional()
+  @ApiProperty({
+    description: 'City',
+    example: 'Springfield',
+    type: 'string',
+  })
+  city: string;
+
+  @IsNotEmpty({ message: 'Longitude is required' })
+  @IsNumber(
+    {
+      allowNaN: false,
+    },
+    { message: 'Longitude must be a number' },
+  )
+  @ApiProperty({
+    description: 'Longitude',
+    example: -123.456,
+    type: 'number',
+  })
+  longitude: number;
+
+  @IsNotEmpty({ message: 'Latitude is required' })
+  @IsNumber(
+    {
+      allowNaN: false,
+    },
+    { message: 'Latitude must be a number' },
+  )
+  @ApiProperty({
+    description: 'Latitude',
+    example: 45.678,
+    type: 'number',
+  })
+  latitude: number;
+
   @IsNotEmpty({ message: 'Details are required' })
   @ValidateNested()
   @Type(() => CarDetailsDto)
