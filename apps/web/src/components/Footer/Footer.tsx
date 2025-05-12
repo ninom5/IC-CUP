62d71051b8c29cca562c258d0dbec7875c13b518
ninom5 <nmilas00@fesb.hr>
2025-05-12 11:14:16 +0200

    Rename classnames

--- apps/web/src/components/Footer/Footer.tsx
+++ apps/web/src/components/Footer/Footer.tsx
@@ -5,12 +5,12 @@ import { koloFooterSvg, lineSvg } from "assets/images/index";
 export const Footer = () => {
   return (
     <section className="footer-section">
-      <div className="ft">
+      <div className="footer-content">
         <div className="kolo-footer-wrapper">
           <img src={koloFooterSvg} alt="Kolo footer logo" />
         </div>
 
-        <div className="a">
+        <div className="footer-columns">
           <Contact />
 
           <SiteMapFooter />
