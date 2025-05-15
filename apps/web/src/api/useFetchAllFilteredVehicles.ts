e88110459054bd1954c7eb430d10cfa064915e4e
ninom5 <nmilas00@fesb.hr>
2025-05-15 04:02:41 +0200

    Fix errors

--- apps/web/src/api/useFetchAllFilteredVehicles.ts
+++ apps/web/src/api/useFetchAllFilteredVehicles.ts
@@ -1,35 +1,47 @@
 import { useQuery } from "@tanstack/react-query";
 import { api } from "./base";
-import { VehicleType } from "types/vehicle.type";
 
-const getAllFilteredVehicles = async (filters?: {
+interface Filters {
   fuelType?: string;
   seats?: string;
   carCategory?: string;
   transmission?: string;
   dateRange?: [Date | null, Date | null];
+}
+
+const getAllFilteredVehicles = async ({
+  queryKey,
+}: {
+  queryKey: [string, Filters?];
 }) => {
+  const [_key, filters] = queryKey;
+
   const params = new URLSearchParams({
     ...(filters?.fuelType && { fuelType: filters.fuelType }),
     ...(filters?.carCategory && { category: filters.carCategory }),
     ...(filters?.transmission && { transmission: filters.transmission }),
     ...(filters?.seats && { seats: filters.seats }),
     ...(filters?.dateRange?.[0] && {
-      startDate: filters?.dateRange?.[0].toISOString(),
+      startDate: filters.dateRange[0].toISOString(),
     }),
     ...(filters?.dateRange?.[1] && {
-      endDate: filters.dateRange?.[1].toISOString(),
+      endDate: filters.dateRange[1].toISOString(),
     }),
   });
 
   return await api.get<never>(`/vehicle/filters?${params.toString()}`);
 };
 
-export const useFetchAllFilteredVehicles = () => {
+export const useFetchAllFilteredVehicles = (filters?: {
+  fuelType?: string;
+  seats?: string;
+  carCategory?: string;
+  transmission?: string;
+  dateRange?: [Date | null, Date | null];
+}) => {
   return useQuery({
-    queryKey: ["vehicle-filters"],
+    queryKey: ["vehicle-filters", filters],
     queryFn: getAllFilteredVehicles,
-    // staleTime: 1000 * 60 * 3,
     staleTime: 0,
     refetchOnWindowFocus: false,
   });
