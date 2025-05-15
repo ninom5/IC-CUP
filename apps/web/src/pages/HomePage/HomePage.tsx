5c4d0657d3d8272d4be6c571c7e91dc01c109ab0
JosipVojkovic <128196492+JosipVojkovic@users.noreply.github.com>
2025-05-15 17:32:04 +0200

    Fix roter layout

--- apps/web/src/pages/HomePage/HomePage.tsx
+++ apps/web/src/pages/HomePage/HomePage.tsx
@@ -3,11 +3,9 @@ import { Link } from "react-router-dom";
 import { Footer, Spinner, Neanderthal } from "@components/index";
 import { kolo } from "assets/videos";
 
-
 export const HomePage = () => {
   return (
     <>
-
       <video loop autoPlay muted playsInline width="100%">
         <source src={kolo} type="video/mp4" />
       </video>
