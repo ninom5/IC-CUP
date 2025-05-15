eb8cfa28999d3dca6663cc13c6cb0e07592ab793
JosipVojkovic <128196492+JosipVojkovic@users.noreply.github.com>
2025-05-15 13:36:31 +0200

    Fix router

--- apps/web/src/Router.tsx
+++ apps/web/src/Router.tsx
@@ -24,33 +24,33 @@ export const Router = () => {
           <Route element={<SearchBarLayout />}>
             <Route path={routes.VEHICLES} element={<VehiclesPage />} />
             <Route path={routes.BECOME_HOST} element={<BecomeHostPage />} />
+          </Route>
+
+          <Route element={<ProtectedRoute />}>
+            <Route path={routes.BECOME_HOST} element={<BecomeHostPage />} />
+            <Route path={routes.ADD_VEHICLE} element={<AddVehiclePage />} />
+
+            <Route element={<SearchBarLayout />}>
+              <Route path={routes.VEHICLE_PAGE} element={<VehiclePage />} />
+            </Route>
+          </Route>
+          <Route element={<AdminRoute />}>
+            <Route path={routes.ADMIN} element={<AdminPage />}></Route>
+          </Route>
+
+          <Route path={routes.BLOCKED} element={<BlockedPage />} />
+
+          <Route element={<NavBarLayout />}>
+            <Route path={routes.HOME} element={<HomePage />} />
+            <Route path={routes.PROFILE_PAGE} element={<ProfilePage />} />
             <Route path={routes.ADD_VEHICLE} element={<AddVehiclePage />} />
             <Route path={routes.USER_VEHICLES} element={<UserVehiclesPage />} />
             <Route
               path={routes.USER_VEHICLE_PROFILE}
               element={<UserVehiclePage />}
             />
+            <Route path={routes.NOT_FOUND} element={<NotFoundPage />} />
           </Route>
-        </Route>
-
-        <Route element={<ProtectedRoute />}>
-          <Route path={routes.BECOME_HOST} element={<BecomeHostPage />} />
-          <Route path={routes.ADD_VEHICLE} element={<AddVehiclePage />} />
-
-          <Route element={<SearchBarLayout />}>
-            <Route path={routes.VEHICLE_PAGE} element={<VehiclePage />} />
-          </Route>
-        </Route>
-        <Route element={<AdminRoute />}>
-          <Route path={routes.ADMIN} element={<AdminPage />}></Route>
-        </Route>
-
-        <Route path={routes.BLOCKED} element={<BlockedPage />} />
-
-        <Route element={<NavBarLayout />}>
-          <Route path={routes.HOME} element={<HomePage />} />
-          <Route path={routes.PROFILE_PAGE} element={<ProfilePage />} />
-          <Route path={routes.NOT_FOUND} element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
