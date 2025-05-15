b2b03f8fe32f0bb2da20cbd555e05d4b4d77b3bb
JosipVojkovic <128196492+JosipVojkovic@users.noreply.github.com>
2025-05-15 21:30:29 +0200

    Add blogs section responsivness

--- apps/web/src/components/Blogs/Blogs.tsx
+++ apps/web/src/components/Blogs/Blogs.tsx
@@ -3,40 +3,42 @@ import c from "./Blogs.module.css";
 export const Blogs = () => {
   return (
     <section className={c.blogsSection}>
-      <div className={c.blog}>
-        <div className={c.blogText}>
-          <h2>Što je kolo i kako je nastalo?</h2>
+      <div className={c.blogsContainer}>
+        <div className={c.blogItem}>
+          <div className={c.blogText}>
+            <h2>ŠTO JE KOLO I KAKO JE NASTALO?</h2>
 
-          <h3>Svaka priča ima svoj početak, a kolo ima dva</h3>
+            <h3>SVAKA PRIČA IMA SVOJ POČETAK, A KOLO IMA DVA</h3>
 
-          <p>
-            Svaka priča ima svoj početak, ali kolo je toliko važno da ima čak
-            dva. Prva priča o kolu počinje još davnih dana kada su ljudi počeli
-            uviđati kako im taj okrugli oblik olakšava život. Nova priča o Kolu
-            počinje u Splitu, gdje se skupina mladih, ambicioznih studenata
-            odlučila na poduzetnički pothvat
-          </p>
+            <p>
+              Svaka priča ima svoj početak, ali kolo je toliko važno da ima čak
+              dva. Prva priča o kolu počinje još davnih dana kada su ljudi
+              počeli uviđati kako im taj okrugli oblik olakšava život. Nova
+              priča o Kolu počinje u Splitu, gdje se skupina mladih, ambicioznih
+              studenata odlučila na poduzetnički pothvat
+            </p>
+          </div>
+
+          <button>SAZNAJ VIŠE</button>
         </div>
 
-        <button>SAZNAJ VIŠE</button>
-      </div>
+        <div className={c.blogItem}>
+          <div className={c.blogText}>
+            <h2>UČINI SVOJ ODMOR POSEBNIM</h2>
 
-      <div className={c.blog}>
-        <div className={c.blogText}>
-          <h2>Učini svoj odmor posebnim</h2>
+            <h3>ZAŠTO IZNAJMITI AUTOMOBIL LOKALACA?</h3>
 
-          <h3>Zašto iznajmiti automobil lokalaca?</h3>
+            <p>
+              Putovanja nisu samo destinacije, to su trenutci koje dijelimo sa
+              samim sobom i drugima. Trenutci nastaju među nama ali zaslužuju
+              imati svoje mjesto. Upravo to je razlog zbog kojeg putovanja
+              lokalnim područjem u automobilu lokalca mijenjaju doživljaj, kako
+              cijelog putovanja tako i same destinacije.
+            </p>
+          </div>
 
-          <p>
-            Putovanja nisu samo destinacije, to su trenutci koje dijelimo sa
-            samim sobom i drugima. Trenutci nastaju među nama ali zaslužuju
-            imati svoje mjesto. Upravo to je razlog zbog kojeg putovanja
-            lokalnim područjem u automobilu lokalca mijenjaju doživljaj, kako
-            cijelog putovanja tako i same destinacije.
-          </p>
+          <button>SAZNAJ VIŠE</button>
         </div>
-
-        <button>SAZNAJ VIŠE</button>
       </div>
     </section>
   );
