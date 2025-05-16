bf301c2e9d93ff7f873323fcd765c3e1e7527007
JosipVojkovic <128196492+JosipVojkovic@users.noreply.github.com>
2025-05-16 12:37:56 +0200

    Add

--- apps/web/src/Router.tsx
+++ apps/web/src/Router.tsx
@@ -22,38 +22,39 @@ export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<MobileLayout />} />
-        <Route element={<SearchBarLayout />}>
-          <Route path={routes.VEHICLES} element={<VehiclesPage />} />
-          <Route path={routes.BECOME_HOST} element={<BecomeHostPage />} />
-          <Route path={routes.ADD_VEHICLE} element={<AddVehiclePage />} />
-          <Route path={routes.USER_VEHICLES} element={<UserVehiclesPage />} />
-          <Route
-            path={routes.USER_VEHICLE_PROFILE}
-            element={<UserVehiclePage />}
-          />
-        </Route>
-
-        <Route element={<ProtectedRoute />}>
-          <Route path={routes.BECOME_HOST} element={<BecomeHostPage />} />
-          <Route path={routes.ADD_VEHICLE} element={<AddVehiclePage />} />
-
+        <Route element={<MobileLayout />}>
           <Route element={<SearchBarLayout />}>
-            <Route path={routes.VEHICLE_PAGE} element={<VehiclePage />} />
+            <Route path={routes.VEHICLES} element={<VehiclesPage />} />
+            <Route path={routes.BECOME_HOST} element={<BecomeHostPage />} />
+            <Route path={routes.ADD_VEHICLE} element={<AddVehiclePage />} />
+            <Route path={routes.USER_VEHICLES} element={<UserVehiclesPage />} />
+            <Route
+              path={routes.USER_VEHICLE_PROFILE}
+              element={<UserVehiclePage />}
+            />
           </Route>
-        </Route>
-        <Route element={<AdminRoute />}>
-          <Route path={routes.ADMIN} element={<AdminPage />}></Route>
-        </Route>
 
-        <Route path={routes.BLOCKED} element={<BlockedPage />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path={routes.BECOME_HOST} element={<BecomeHostPage />} />
+            <Route path={routes.ADD_VEHICLE} element={<AddVehiclePage />} />
 
-        <Route element={<NavBarLayout />}>
-          <Route path={routes.HOME} element={<HomePage />} />
-          <Route path={routes.PROFILE_PAGE} element={<ProfilePage />} />
-          <Route path={routes.NOT_FOUND} element={<NotFoundPage />} />
-          <Route path={routes.REGISTER_PAGE} element={<RegisterPage />} />
-          <Route path={routes.LOGIN_PAGE} element={<LoginPage />} />
+            <Route element={<SearchBarLayout />}>
+              <Route path={routes.VEHICLE_PAGE} element={<VehiclePage />} />
+            </Route>
+          </Route>
+          <Route element={<AdminRoute />}>
+            <Route path={routes.ADMIN} element={<AdminPage />}></Route>
+          </Route>
+
+          <Route path={routes.BLOCKED} element={<BlockedPage />} />
+
+          <Route element={<NavBarLayout />}>
+            <Route path={routes.HOME} element={<HomePage />} />
+            <Route path={routes.PROFILE_PAGE} element={<ProfilePage />} />
+            <Route path={routes.NOT_FOUND} element={<NotFoundPage />} />
+            <Route path={routes.REGISTER_PAGE} element={<RegisterPage />} />
+            <Route path={routes.LOGIN_PAGE} element={<LoginPage />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
